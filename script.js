4221cb07d9fcf0e1bb6ec2cdcb7e91038d3e6a00
shinjp720 <shinjp720@gmail.com>
2025-06-04 21:15:20 +0900

    Updated many

--- script.js
+++ script.js
@@ -1,74 +1,99 @@
-// ファイルが大きくて正常にリロードできない問題の対策
-window.addEventListener("DOMContentLoaded", () => {
-    if (location.hash) {
-        const target = document.querySelector(location.hash);
-        if (target) {
-            target.scrollIntoView();
+const naviUrl = "./layouts/navi.html";
+
+main();
+
+async function main() {
+    // history.replaceState(null, "", location.pathname);
+    const naviContents = await getNavi(naviUrl);
+    document.getElementById("navi").innerHTML = naviContents;
+    addLinkListeners();
+}
+
+// navi情報を取得して返す
+async function getNavi(naviUrl) {
+    try {
+        const response = await fetch(naviUrl);
+        if (!response.ok) {
+            throw new Error("naviファイルが読み込めません");
         }
+        return await response.text();
+    } catch (error) {
+        document.getElementById("navi").textContent = error;
+        return "";
     }
-});
+}
 
-
-// naviの読み込み処理
-function addLinkListeners() {
+// naviにイベントを追加
+async function addLinkListeners() {
     const links = document.querySelectorAll(".md-link");
     links.forEach(link => {
-        link.addEventListener("click", event => {
+        link.addEventListener("click", async (event) => {
             event.preventDefault();
-            const path = event.currentTarget.getAttribute("href");
-            convertMd(path);
+            const href = event.currentTarget.getAttribute("href");
+            await loadContents(href);
         });
     });
 }
 
-fetch("./layouts/navi.html")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("naviファイルが読み込みません");
-        }
-        return response.text();
-    })
-    .then(navi => {
-        document.getElementById("navi").innerHTML = navi;
-        addLinkListeners();
-    })
-    .catch(error => {
-        document.getElementById("navi").textContent = error;
-    });
+async function loadContents(href) {
+    const processedHtml = await convertMd(`${href}.md`);
+    history.pushState(null, "", href);
+    document.getElementById("main").innerHTML = processedHtml;
 
-
-// .mdの変換処理
-function convertMd(path) {
-    fetch(`./pages/${path}.md`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("mdファイルが読み込めません");
-            }
-            return response.text();
-        })
-        .then(markdown => {
-            const html = marked.parse(markdown);
-            document.getElementById("main").innerHTML = html;
-            importAnchors();
-        })
-        .catch(error => {
-            document.getElementById("main").textContent = error;
-        })
+    const tocContents = importAnchors(href);
+    document.getElementById("toc").innerHTML = "";
+    document.getElementById("toc").appendChild(tocContents);
 }
 
+// .mdの変換処理
+async function convertMd(href) {
+    try {
+        const response = await fetch(href);
+        if (!response.ok) {
+            throw new Error("mdファイルが読み込めません");
+        }
+
+        const mdText = await response.text();
+        const processedHtml = marked.parse(mdText);
+
+        return processedHtml;
+    } catch (error) {
+        document.getElementById("main").textContent = error;
+        return "";
+    }
+}
 
 // tocの読み込み処理
-function importAnchors() {
-    const ulTag = document.querySelector("#toc ul");
-    ulTag.innerHTML = ""; // ulタグをリセット
+function importAnchors(href) {
+    const ulTag = document.createElement("ul");
     const anchors = document.querySelectorAll("[data-name]");
-
     anchors.forEach(anchor => {
         let newA = document.createElement("a");
         newA.textContent = anchor.dataset.name;
-        newA.href = `#${anchor.id}`;
+        newA.addEventListener("click", (e) => {
+            e.preventDefault();
+            document.getElementById(anchor.id).scrollIntoView({ behavior: "smooth", block: "start" });
+        })
+        // newA.href = `#${anchor.id}`;
         let newLi = document.createElement("li");
         newLi.appendChild(newA);
         ulTag.appendChild(newLi);
     })
+    return ulTag;
 }
+
+window.addEventListener('popstate', () => {
+    const href = location.pathname;
+    loadContents(href);
+    // document.getElementById('content').innerHTML = html;
+});
+
+// ファイルが大きくて正常にリロードできない問題の対策
+// window.addEventListener("DOMContentLoaded", () => {
+//     if (location.hash) {
+//         const target = document.querySelector(location.hash);
+//         if (target) {
+//             target.scrollIntoView();
+//         }
+//     }
+// });
